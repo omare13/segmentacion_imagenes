dd61250cb5c2f21eb1b5f9be1ca1137dcf58b6eb
omare13 <darknessit@msn.com>
2019-05-31 20:07:43 +0200

    Funcionamiento de guardado/carga de imágenes. Incluye funcionamiento de botones previo y siguiente.

--- ventana_inicial.py
+++ ventana_inicial.py
@@ -2,6 +2,7 @@ from ventana_inicial_ui import *
 import os
 import re
 import clases
+import pickle
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -23,6 +24,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Menú
         self.actionAbrir_Imagen.triggered.connect(self.abrir_imagen)
         self.actionAbrir_Carpeta.triggered.connect(self.abrir_carpeta)
+        self.actionGuardar.triggered.connect(self.guardar_imagen)
 
         self.carpeta = None
         self.imagenes = None
@@ -63,33 +65,63 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # Diálogo de selección de carpeta
         self.carpeta = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Seleccionar fichero"))
-        print("CARPETA SELECCIONADA", self.carpeta)
+        if len(self.carpeta) > 0:
+            print("CARPETA SELECCIONADA", self.carpeta)
 
-        # Inicializamos las imágenes de la carpeta
-        self.imagenes = []
+            # Inicializamos las imágenes de la carpeta
+            self.imagenes = []
 
-        # Comprobamos que hay imágenes y las recuperamos
-        for fichero in os.listdir(self.carpeta):
-            extension = re.match(r'(.*)(\.[a-zA-Z0-9]{1,5})', fichero)
-            nombre_fichero = extension.group(1)
-            extension_fichero = extension.group(2)
-            if extension_fichero in formatos_validos:
-                imagen = clases.Imagen(ruta=self.carpeta + "/" + fichero, nombre=nombre_fichero,
-                                       formato=extension_fichero)
-                self.imagenes.append(imagen)
+            # Comprobamos que hay imágenes y las recuperamos
+            for fichero in os.listdir(self.carpeta):
+                extension = re.match(r'(.*)(\.[a-zA-Z0-9]{1,5})', fichero)
+                nombre_fichero = extension.group(1)
+                extension_fichero = extension.group(2)
+                if extension_fichero in formatos_validos:
+                    imagen = clases.Imagen(ruta=self.carpeta + "/" + fichero, nombre=nombre_fichero,
+                                           formato=extension_fichero)
+                    self.imagenes.append(imagen)
 
-        if len(self.imagenes) > 0:
-            print("CARGA DE " + str(len(self.imagenes)) + " IMÁGENES")
-            self.imagen_actual = 0
-            print("IMAGEN ACTUAL ", self.imagen_actual)
-            self.cargar_imagen()
-        else:
-            self.imagenes = None
-            print("NO SE ENCONTRARON IMÁGENES")
+            if len(self.imagenes) > 0:
+                print("CARGA DE " + str(len(self.imagenes)) + " IMÁGENES")
+                self.imagen_actual = 0
+                print("IMAGEN ACTUAL ", self.imagen_actual)
+                self.cargar_imagen()
+            else:
+                self.imagenes = None
+                print("NO SE ENCONTRARON IMÁGENES")
 
     def abrir_imagen(self):
         print(self.imagen_actual)
         print("ABRIR IMAGEN")
+
+        # Diálogo de selección de imagen
+        ruta_imagen = str(QtWidgets.QFileDialog.getOpenFileName(self, caption="Seleccionar imagen",
+                                                                filter="Images (*.png *.xpm *.jpg)"))
+        ruta_imagen = (ruta_imagen[2:ruta_imagen.rfind(",") - 1])
+
+        if len(ruta_imagen) > 0:
+            self.carpeta = ruta_imagen[:ruta_imagen.rfind('/')]
+            fichero = ruta_imagen[ruta_imagen.rfind('/')+1:]
+            extension = re.match(r'(.*)(\.[a-zA-Z0-9]{1,5})', fichero)
+            nombre_fichero = extension.group(1)
+            extension_fichero = extension.group(2)
+
+            # Creamos el objeto imagen y lo incluimos
+            self.imagenes = []
+            imagen = clases.Imagen(ruta=self.carpeta + "/" + fichero, nombre=nombre_fichero,
+                                   formato=extension_fichero)
+            self.imagenes.append(imagen)
+            print(imagen.__dict__)
+
+            # Chequeamos que se haya cargado la imagen
+            if len(self.imagenes) > 0:
+                print("CARGA DE " + str(len(self.imagenes)) + " IMÁGENES")
+                self.imagen_actual = 0
+                print("IMAGEN ACTUAL ", self.imagen_actual)
+                self.cargar_imagen()
+            else:
+                self.imagenes = None
+                print("NO SE ENCONTRARON IMÁGENES")
 
     def cargar_imagen(self):
         print("CARGAR IMAGEN")
@@ -101,10 +133,41 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.escena.addPixmap(imagen)
         print("CARGADA IMAGEN ", self.imagenes[self.imagen_actual].nombre)
 
+        # Cargamos datos posiblemente guardados en otra ocasión
+        self.cargar_puntos()
+
+        # Dibujamos los elementos que se pudieron guardar
+        self.dibujar_puntos()
+
+    def cargar_puntos(self):
+        print("CARGAR MODELO")
         # Inicializamos/Cargamos los puntos y segmentos que pudieran haber sido guardados previamente
-        self.puntos = []
-        self.segmentos = []
-        # TODO - Incluir la forma de cargar puntos y segmentos previamente incluidos (de una imagen guardada)
+        path = self.carpeta
+        ocurrencias = [i for i, char in enumerate(path) if char == "/"]
+        print(ocurrencias)
+        nombre_carpeta = path[ocurrencias[-1]+1:]
+        carpeta_anotaciones = path[:ocurrencias[-2]]+"/ANOTACIONES/" + nombre_carpeta + "/"
+
+        fichero_puntos = self.imagenes[self.imagen_actual].nombre+"_puntos.pickle"
+        fichero_segmentos = self.imagenes[self.imagen_actual].nombre+"_segmentos.pickle"
+
+        if fichero_puntos in os.listdir(carpeta_anotaciones):
+            print("ENCONTRADOS PUNTOS")
+            with open(carpeta_anotaciones + fichero_puntos, "rb") as file:
+                self.puntos = pickle.load(file)
+        else:
+            self.puntos = []
+            print("NO SE ENCONTRARON PUNTOS GUARDADOS")
+
+        if fichero_segmentos in os.listdir(carpeta_anotaciones):
+            print("ENCONTRADOS SEGMENTOS")
+            with open(carpeta_anotaciones + fichero_segmentos, "rb") as file:
+                self.segmentos = pickle.load(file)
+        else:
+            self.segmentos = []
+            print("NO SE ENCONTRARON PUNTOS GUARDADOS")
+
+        self.rellenar_listas()
 
     def add_punto(self, coord_x, coord_y, elemento):
         print("AÑADIR PUNTO")
@@ -150,11 +213,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def imagen_previa(self):
         if self.imagen_actual > 0:
+            self.guardar_imagen()
+            self.desmontar_imagen()
+            self.reset_modelo()
+            self.vaciar_listas()
             self.imagen_actual -= 1
             self.cargar_imagen()
 
     def imagen_siguiente(self):
         if self.imagen_actual < (len(self.imagenes) - 1):
+            self.guardar_imagen()
+            self.desmontar_imagen()
+            self.reset_modelo()
+            self.vaciar_listas()
             self.imagen_actual += 1
             self.cargar_imagen()
 
@@ -162,6 +233,68 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Cambio el estado a inicial y el cursor por defecto
         self.estado = "INIT"
         self.frame_edicion.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+
+    def guardar_imagen(self):
+        print("GUARDAR IMAGEN", self.imagen_actual)
+
+        path = self.carpeta
+        ocurrencias = [i for i, char in enumerate(path) if char == "/"]
+        print(ocurrencias)
+        nombre_carpeta = path[ocurrencias[-1] + 1:]
+        carpeta_anotaciones = path[:ocurrencias[-2]] + "/ANOTACIONES/" + nombre_carpeta + "/"
+
+        fichero_puntos = self.imagenes[self.imagen_actual].nombre + "_puntos.pickle"
+        fichero_segmentos = self.imagenes[self.imagen_actual].nombre + "_segmentos.pickle"
+        print(fichero_puntos)
+        print(fichero_segmentos)
+
+        if len(self.puntos) > 0:
+            with open(carpeta_anotaciones + fichero_puntos, 'wb') as file:
+                pickle.dump(self.puntos, file)
+
+        if len(self.segmentos) > 0:
+            with open(carpeta_anotaciones + fichero_segmentos, 'wb') as file:
+                pickle.dump(self.segmentos, file)
+
+        # TODO - Los paso a grafo
+        fichero_grafo = self.self.imagenes[self.imagen_actual].nombre + "_grafo.txt"
+        ruta_fichero_grafo = carpeta_anotaciones + fichero_grafo
+
+    def desmontar_imagen(self):
+        for item_escena in self.escena.items():
+            if type(item_escena) == QtWidgets.QGraphicsPixmapItem:
+                self.escena.removeItem(item_escena)
+
+    def vaciar_listas(self):
+        listas = [self.lista_segmentos, self.lista_puntos]
+        for lista in listas:
+            lista.clear()
+
+    def rellenar_listas(self):
+        for punto in self.puntos:
+            # Añado el punto a la lista de puntos
+            print("INCLUIR PUNTO EN LISTA DE PUNTOS")
+            item_punto = QtWidgets.QListWidgetItem(self.lista_puntos)
+            item_punto.setText(punto.nombre)
+        for segmento in self.segmentos:
+            # Añado el punto a la lista de puntos
+            print("INCLUIR SEGMENTO EN LISTA DE SEGMENTOS")
+            item_punto = QtWidgets.QListWidgetItem(self.lista_segmentos)
+            item_punto.setText(segmento.nombre)
+
+    def dibujar_puntos(self):
+        for punto in self.puntos:
+            self.escena.dibujar_punto(punto)
+
+        for segmento in self.segmentos:
+            self.escena.dibujar_segmento(segmento)
+
+    def reset_modelo(self):
+        self.estado = "INIT"
+        self.puntos = None
+        self.segmentos = None
+        self.n_puntos = 0
+        self.n_segmentos = 0
 
 
 class MyGraphicsScene(QtWidgets.QGraphicsScene):
@@ -198,7 +331,7 @@ class MyGraphicsScene(QtWidgets.QGraphicsScene):
             # Si estamos en creación de segmento y se hace click izquierdo, generamos el path desde el punto anterior
             elif (QGraphicsSceneMouseEvent.button() == 1) and (self.creacion_segmento is True):
                 self.removeItem(self.recta_apoyo)
-                print("CONTINUA EDICIÓN SEGMENTO")
+                print("CONTINÚA EDICIÓN SEGMENTO")
                 self.path.lineTo(QGraphicsSceneMouseEvent.scenePos())
                 self.removeItem(self.path_item)
                 self.path_item = self.addPath(self.path, pen=self.path_pen)
@@ -255,6 +388,24 @@ class MyGraphicsScene(QtWidgets.QGraphicsScene):
         self.path = None
         self.path_item = None
         self.creacion_segmento = False
+
+    def dibujar_punto(self, punto):
+        # Configuro el pincel y la brocha
+        pen = QtGui.QPen(QtCore.Qt.black)
+        brush = QtGui.QBrush(QtCore.Qt.black)
+
+        # Obtengo las coordenadas del evento de click
+        x = punto.x
+        y = punto.y
+
+        # Dibujo el punto
+        elipse = self.addEllipse(x, y, 4, 4, pen, brush)
+
+    def dibujar_segmento(self, segmento):
+        path = QtGui.QPainterPath(QtCore.QPointF(segmento.puntos[0].x, segmento.puntos[0].y))
+        for punto in segmento.puntos[1:]:
+            path.lineTo(QtCore.QPointF(punto.x, punto.y))
+        self.addPath(path, pen=self.path_pen)
 
 
 if __name__ == "__main__":
